4a4f9024ad5d08449b5d3071ec3cc008a0cfb04c
Lua-sketch <luanna.v.quinalha@gmail.com>
2021-03-03 13:22:36 -0300

    added nav dropdown, initial page

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,8 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li')
+  const nav = document.querySelector('.navbar');
+  const navLinks = document.querySelectorAll('.navbar .dropdown button')
+  const navLinksExtra = document.querySelectorAll('.navbar .dropdown a')
   burger.addEventListener("click", () => {
         //Toggle Nav
         nav.classList.toggle("nav-active");
@@ -13,6 +14,14 @@ const navSlide = () => {
             } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
+        });
+
+        navLinksExtra.forEach((link, index)=>{
+          if (link.style.animation) {
+              link.style.animation = ""
+          } else {
+              link.style.animation = `navLinkExtraFade ease forwards`;
+          }
         });
         //Burger Animation
         burger.classList.toggle("toggle");
