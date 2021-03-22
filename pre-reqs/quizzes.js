d29ce7c2494b7dd477671dd6d7a3766e9001f9ac
Lua-sketch <luanna.v.quinalha@gmail.com>
2021-03-22 08:00:33 -0300

    favicon and quiz

---
+++ pre-reqs/quizzes.js
@@ -0,0 +1,158 @@
+(function(){
+  // Functions
+  function buildQuiz(){
+    // variable to store the HTML output
+    const output = [];
+
+    // for each question...
+    myQuestions1.forEach(
+      (currentQuestion, questionNumber) => {
+
+        // variable to store the list of possible answers
+        const answers = [];
+
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
+
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+        // add this question and its answers to the output
+        output.push(
+          `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+        );
+      }
+    );
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer1.innerHTML = output.join('');
+  }
+
+  function showResults(){
+
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer1.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions1.forEach( (currentQuestion, questionNumber) => {
+
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if(userAnswer === currentQuestion.correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer1.innerHTML = `${numCorrect} de ${myQuestions1.length}`;
+  }
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton1.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton1.style.display = 'none';
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+  // Variables
+  const quizContainer1 = document.getElementById('quiz1');
+  const resultsContainer1 = document.getElementById('results1');
+  const submitButton1 = document.getElementById('submit1');
+  const myQuestions1 = [
+    {
+      question: "Qual o raio do círculo trigonométrico?",
+      answers: {
+        a: "10",
+        b: "1",
+        c: "qualquer raio (não importa)"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "O que representa o eixo x do ciclo trigonométrico? E o eixo y?",
+      answers: {
+        a: "x: seno, y: cosseno",
+        b: "x: tangente, y: cotangente",
+        c: "x: cosseno, y: seno"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Como podemos encontrar a tangente de um ângulo?",
+      answers: {
+        a: "tangente = (cateto oposto)/(cateto adjacente)",
+        b: "tangente = (cateto adjacente)/(hipotenusa)",
+        c: "tangente = (seno)/(cosseno)",
+        d: "'a' e 'c' estão corretos"
+      },
+      correctAnswer: "d"
+    }
+    {
+      
+    }
+  ];
+
+  // Kick things off
+  buildQuiz();
+
+  // Pagination
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  // Show the first slide
+  showSlide(currentSlide);
+
+  // Event listeners
+  submitButton1.addEventListener('click', showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+})();
