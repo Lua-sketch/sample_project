4e12db63371e2bc4d6f0307f93064c5f53c68d1a
Luanna Veroneze <luanna.v.quinalha@gmail.com>
2021-04-27 10:43:35 -0300

    added quiz 4

--- quantum-mechanics/quizzes.js
+++ quantum-mechanics/quizzes.js
@@ -109,20 +109,80 @@
   const submitButton4 = document.getElementById('submit4');
   const myQuestions4 = [
     {
-      question:"testing",
+      question:"A mecânica quântica é usada em",
       answers:{
-        a:"a",
-        b:"b",
-        c:"c"
+        a:"Relógios atômicos e GPS",
+        b:"Smartphones/computadores modernos",
+        c:"Telecomunicações",
+        d:"Todos os anteriores"
+      },
+      correctAnswer:"d"
+    },
+    {
+      question:"A luz é feita de",
+      answers:{
+        a:"Ondas",
+        b:"Partículas",
+        c:"Ambos"
+      },
+      correctAnswer:"c"
+    },
+    {
+      question:"Se você deixa luz passar por duas finas fendas, e observa o resultado final, você vê",
+      answers:{
+        a:"Duas linhas iluminadas, uma atrás de cada fenda",
+        b:"Várias linhas iluminadas e outras sem luz, um padrão de interferência",
+        c:"A tela inteira iluminada uniformemente",
+        d:"Nenhuma luz, ela foi bloqueada"
+      },
+      correctAnswer:"b"
+    },
+    {
+      question:"Para retirar elétrons de um átomo,",
+      answers:{
+        a:"Podemos usar bastante luz de baixa energia ou menos luz de alta energia",
+        b:"Precisamos de luz com uma energia alta o suficiente",
+        c:"Não podemos retirar elétrons de um átomo"
+      },
+      correctAnswer:"b"
+    },
+    {
+      question:"A velocidade de uma partícula em função de sua massa e comprimento de onda é",
+      answers:{
+        a:"v = h/(m*λ)",
+        b:"v = (m*h)/λ",
+        c:"v = (λ*h)/m",
+        d:"v = m*λ*h"
       },
       correctAnswer:"a"
     },
     {
-      question:"testing again",
+      question:"Sobre a função de onda de uma partícula, é incorreto afirmar que",
       answers:{
-        a:"a",
-        b:"b",
-        c:"c"
+        a:"Ela depende de x e t",
+        b:"Ela nos fornece a posição de uma partícula em determinado momento",
+        c:"Sua evolução temporal é descrita pela equação de Schrodinger",
+        d:"Todas as anteriores"
+      },
+      correctAnswer:"b"
+    },
+    {
+      question:"O operador Hamiltaniano",
+      answers:{
+        a:"Nos fornece o momento linear de uma partícula",
+        b:"Não influencia a evolução temporal da função de onda de uma partícula",
+        c:"Nos fornece a energia cinética de uma partícula",
+        d:"Nos fornece a energia total de uma partícula"
+      },
+      correctAnswer:"d"
+    },
+    {
+      question:"Uma partícula tem maior probabilidade de se encontrar onde",
+      answers:{
+        a:"Ela tem menos energia",
+        b:"Sua função de onda tem a maior amplitude",
+        c:"Ela tem menos momento linear",
+        d:"Nenhuma das anteriores"
       },
       correctAnswer:"b"
     }
